bfeee7f2b4e5959bd1f48ea7b04d6b768574846a
DPMadhavan <71888958+DPMadhavan@users.noreply.github.com>
2021-03-10 19:39:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
-});
-
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
